ed3866366afc3dcd75fbd5d16b094785d2bc884b
Ilia Parunashvili <iparunashvili123@gmail.com>
2020-07-03 07:33:21 -0400

    styled login

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__);
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/sign-up')
 def get_started():
-    return render_template('login.html', action='Sign up', link='Sign in')
+    return render_template('login.html', action='Sign up', link='Sign in', question="Already have an account?", title1='Create', title2='account')
 
 @app.route('/sign-in')
 def login():
-    return render_template('login.html', action='Sign in', link='Sign up')
+    return render_template('login.html', action='Sign in', link='Sign up', question="Don't have an account?", title1='Welcome', title2='back')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/login.js
+++ static/login.js
@@ -6,23 +6,32 @@ const passIn = document.querySelector('#pass-input');
 const signInBtn = document.querySelector('#sign-in');
 const signUpBtn = document.querySelector('#sign-up');
 const link = document.querySelector('#link');
+const question = document.querySelector('#question');
+const title_h1 = document.querySelector('#title');
 
 function whichOne(title) {
   if (title === 'Animusic | Sign in') {
+    document.title = 'Animusic | Sign in';
     nameIn.style.display = 'none'
     signInBtn.style.display = 'block';
     signUpBtn.style.display = 'none';
     link.innerText = 'Sign up';
+    question.innerText = "Don't have an account? ";
+    title_h1.innerHTML = 'Welcome<br> back';
   } else {
+    document.title = 'Animusic | Sign up';
+    nameIn.style.display = 'block'
     signUpBtn.style.display = 'block';
     signInBtn.style.display = 'none';
     link.innerText = 'Sign in';
+    question.innerText = "Already have an account? ";
+    title_h1.innerHTML = 'Create<br> account';
   }
 }
 whichOne(document.title)
 
 function pushState(page, title) {
-  window.history.replaceState({}, title, `${window.origin}/${page}`);
+  window.history.pushState({}, title, `${window.origin}/${page}`);
   whichOne(title)
 }
 
@@ -35,7 +44,7 @@ function signUp() {
     .then(() => database.ref('/users/' + auth.currentUser.uid).set({ full_name: name }))
 }
 
-function signUp() {
+function signIn() {
   var email = emailIn.value;
   var pass = passIn.value;
   auth.signInWithEmailAndPassword(email, pass)
